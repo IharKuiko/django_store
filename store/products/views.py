3fc6157cf24329928f94df0578346427d5833b8e
Ihar Kuiko <coooll81@gmail.com>
2023-05-29 21:54:09 +0200

    add tamplates

--- store/products/views.py
+++ store/products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Test Title',
+        'username': 'Ihar'
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    return render(request, 'products/products.html')
